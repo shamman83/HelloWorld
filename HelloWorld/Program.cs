aa828f183010e9f8f4dc87225938a7dd55148fd4
micha <micha@DESKTOP-1SBJN08>
2023-08-09 17:27:20 +0200

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace HelloWorld
+{
+    //PascalCase
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //camelClass
+            string firstAndLastName  = "Michał";
+
+            //UPPER_CASE
+            const double PI_NUMBER = 3.14;
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Nazywam się Michał Kamiński");
+            if (PI_NUMBER > 2)
+            {
+                WritePiNumber(PI_NUMBER);
+            }
+        }
+
+        private static string WritePiNumber(double pi)
+        {
+            return ("3.14");
+        }
+    }
+}
+
+ 
